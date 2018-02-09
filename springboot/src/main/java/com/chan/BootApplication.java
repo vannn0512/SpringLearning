e5c8042a4f393d5025795b692c6080f2bf6ea0c9
vannn0512 <vanjunchen@googlemail.com>
2018-02-09 14:56:03 +0800

    添加druid配置

--- springboot/src/main/java/com/chan/BootApplication.java
+++ springboot/src/main/java/com/chan/BootApplication.java
@@ -2,10 +2,7 @@ package com.chan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class BootApplication {
 
